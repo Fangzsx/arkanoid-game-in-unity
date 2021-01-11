8026f8095bde1e5173f513ecebe6627769aaefcf
Fangszx <jygrzn@gmail.com>
2021-01-11 21:10:28 +0800

    Recreated a new project

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    //configuration parameters
+    [SerializeField] float ScreenWidthUnit =16f;
+    [SerializeField] float PaddleMinPos = 1f;
+    [SerializeField] float PaddleMaxPos = 15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXpos = Input.mousePosition.x / Screen.width * ScreenWidthUnit;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseXpos, PaddleMinPos, PaddleMaxPos);
+
+        transform.position = paddlePos;
+    }
+}
