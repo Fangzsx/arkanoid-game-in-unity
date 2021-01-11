99a26450b9b091152c8da1715130ccce4d0e563e
Fangszx <jygrzn@gmail.com>
2021-01-11 21:46:06 +0800

    Paddle and Ball Update
    Sticked the ball into the paddle via Offset
    Paddle movement clamped to the dimension of the screen.

--- Block Breaker/Assets/Ball.cs
+++ Block Breaker/Assets/Ball.cs
@@ -7,11 +7,13 @@ public class Ball : MonoBehaviour
 
     //config params
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
 
     //state
     Vector2 paddleToBallVector;
-
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,34 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+
+        /*
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        */
+
+         LaunchOnMouseClick();
+         if(hasStarted == false)
+        {
+            LockBallToPaddle();
+        }
+    }
+
+
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
