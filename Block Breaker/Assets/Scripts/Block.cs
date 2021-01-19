a0da9418c739c83dfd4573022ebec88d283d0bcb
Fangszx <jygrzn@gmail.com>
2021-01-19 13:08:49 +0800

    added 2hit and 3hit blocks using Sprites
    Changed sprite of a block using GetComponent<SpriteRenderer>().sprite();

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -1,24 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
 
+    // config params
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
 
     //cache references
     Level level;
+
+    //state variables
+    [SerializeField] int timesHit;
     
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(tag == "Breakable")
         {
+            HandleHit();
+        }
+
+
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+
             DestroyBlock();
         }
-        
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
 
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
