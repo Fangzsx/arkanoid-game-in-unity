4042b22786cdfa7879ef34c9aae21fa984363609
Fangszx <jygrzn@gmail.com>
2021-01-22 18:09:31 +0800

    Updated Levels

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
@@ -10,6 +8,7 @@ public class Ball : MonoBehaviour
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
     [SerializeField] AudioClip[] clips;
+    [SerializeField] float randomFactor;
 
 
     //cached component references
@@ -54,10 +53,16 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        Vector2 velocityTweak = new Vector2
+            (Random.Range(0f, randomFactor),
+            Random.Range(0f, randomFactor));
+
         if (hasStarted) 
         {
-            AudioClip clip = clips[UnityEngine.Random.Range(0, clips.Length)];
+            AudioClip clip = clips[Random.Range(0, clips.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
         }
         
     }
